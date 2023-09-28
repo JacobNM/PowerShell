fab8bcc3624847982ab50f44a3ffba91fb262cce
JacobNM <133252538+JacobNM@users.noreply.github.com>
2023-09-28 13:18:17 -0400

    Progress bar successful

--- Active Directory/Creating a Progress bar.ps1
+++ Active Directory/Creating a Progress bar.ps1
@@ -5,7 +5,13 @@ $inner=@(1..100)
 
 $counter=1
 foreach($x in $outer){
-    Write-Progress -Id 1 -Activity "Progress" -Status "$(($counter/$outer.Count)*100)% Complete" -PercentComplete $(($counter/$outer.Count)*100)
+    $innercounter=1
+    foreach($y in $inner){
+        Write-Progress -Id 2 -Activity "Inner Loop" -Status "$(($innercounter/$inner.Count)*100)% Complete" -PercentComplete $(($innercounter/$inner.Count)*100)
+    $innercounter++
+    Start-Sleep -Milliseconds 100
+    }
+    Write-Progress -Id 1 -Activity "Outer Loop" -Status "$(($counter/$outer.Count)*100)% Complete" -PercentComplete $(($counter/$outer.Count)*100)
     $counter++
     Start-Sleep -Milliseconds 100
 }
