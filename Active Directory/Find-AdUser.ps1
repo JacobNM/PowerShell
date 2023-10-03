2be03296d3cf8b0fd8043f3e012026f98f3cd2a2
JacobNM <133252538+JacobNM@users.noreply.github.com>
2023-10-03 12:11:45 -0400

    Function to find current AD users

---
+++ Active Directory/Find-AdUser.ps1
@@ -0,0 +1,18 @@
+function Find-ADUser {
+    $ADUserFirstName = Read-Host -Prompt "Please provide the first name of the Active Directory user you are searching for"
+    "`nYour results are being produced"
+    Get-ADUser -Filter "givenname -like '$ADUserFirstName'" -Properties "Description"
+# User has option to enter their own filter option with the associated info if they require more searching
+    $UserSearchFilterPrompt = Read-Host -Prompt "`nIs there a different filter you would like to use?"
+
+    if ($UserSearchFilterPrompt -eq "yes" -or "y") {
+        $ADUserFilter = Read-Host -Prompt "`nPlease enter the name of the filter type you would like to use" ; 
+        $ADUserFilterInfo = Read-Host -Prompt "Type the information for your selected filter" ; "`n"
+        Get-ADUser -Filter {$ADUserFilter -like $ADUserFilterInfo} -Properties "Description"
+        "`nSearch complete! Hope this was helpful."
+        }
+    
+    elseif ($UserSearchFilterPrompt -eq "no" -or "n"){
+        Write-Host "`nSearch complete! Hope this was helpful."
+        }
+}
