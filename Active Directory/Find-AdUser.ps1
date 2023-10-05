8875472faa431d004d6f101b917b0212035a667f
JacobNM <133252538+JacobNM@users.noreply.github.com>
2023-10-05 09:47:36 -0400

    Fixed Elseif statement

--- Active Directory/Find-AdUser.ps1
+++ Active Directory/Find-AdUser.ps1
@@ -1,19 +1,18 @@
 function Find-ADUser {
-        $ADUserFirstName = Read-Host -Prompt "Please provide the first name of the Active Directory user you are searching for"
+        ${ADUserFirstName} = Read-Host -Prompt "Please provide the first name of the Active Directory user you are searching for"
         "`nYour results are being produced"
         Get-ADUser -Filter "givenname -like '$ADUserFirstName'" -Properties "Description"
 # User has option to enter their own filter option with the associated info if they require more searching
-        $UserSearchFilterPrompt = Read-Host -Prompt "`nIs there a different filter you would like to use?"
+        ${UserSearchFilterPrompt} = Read-Host -Prompt "`nIs there a different filter you would like to use?"
 
-        if ($UserSearchFilterPrompt -eq "yes" -or "y") {
-            $ADUserFilter = Read-Host -Prompt "`nPlease enter the name of the filter type you would like to use (E.g. surname, department, samaccountname)" ; 
-            $ADUserFilterInfo = Read-Host -Prompt "Type the information for your selected filter" ;
+        if (($UserSearchFilterPrompt) -eq "yes" -or ($UserSearchFilterPrompt) -eq "y") {
+            ${ADUserFilter} = Read-Host -Prompt "`nPlease enter the name of the filter type you would like to use (E.g. surname, department, samaccountname)" 
+            ${ADUserFilterInfo} = Read-Host -Prompt "Type the information for your selected filter"
             "`nYour results are being produced`n"
             Get-ADUser -Filter {$ADUserFilter -like $ADUserFilterInfo} -Properties "Description"
             "`nSearch complete! Hope this was helpful."
-            }
-        
-        elseif ($UserSearchFilterPrompt -eq "no" -or "n"){
+            } 
+            elseif (($UserSearchFilterPrompt) -eq "no" -or ($UserSearchFilterPrompt) -eq "n") {
             Write-Host "`nSearch complete! Hope this was helpful."
             }
     }
