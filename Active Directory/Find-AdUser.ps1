5785561662f7090ab26ba90d7ce2f1e0c8e87114
JacobNM <133252538+JacobNM@users.noreply.github.com>
2023-10-12 11:54:38 -0400

    "Department" added to info produced

--- Active Directory/Find-AdUser.ps1
+++ Active Directory/Find-AdUser.ps1
@@ -1,7 +1,7 @@
 function Find-ADUser {
         ${ADUserFirstName} = Read-Host -Prompt "Please provide the first name of the Active Directory user you are searching for"
         "`nYour results are being produced"
-        Get-ADUser -Filter "givenname -like '$ADUserFirstName'" -Properties "Description","OfficePhone","telephoneNumber"
+        Get-ADUser -Filter "givenname -like '$ADUserFirstName'" -Properties "Department","Description","OfficePhone","telephoneNumber"
 # User has option to enter their own filter option with the associated info if they require more searching
         ${UserSearchFilterPrompt} = Read-Host -Prompt "`nIs there a different filter you would like to use?"
 
@@ -9,7 +9,7 @@ function Find-ADUser {
             ${ADUserFilter} = Read-Host -Prompt "`nPlease enter the name of the filter type you would like to use (E.g. surname, department, samaccountname)" 
             ${ADUserFilterInfo} = Read-Host -Prompt "Type the information for your selected filter"
             "`nYour results are being produced`n"
-            Get-ADUser -Filter {$ADUserFilter -like $ADUserFilterInfo} -Properties "Description","OfficePhone","telephoneNumber"
+            Get-ADUser -Filter {$ADUserFilter -like $ADUserFilterInfo} -Properties "Department","Description","OfficePhone","telephoneNumber"
             "`nSearch complete."
             } 
             elseif (($UserSearchFilterPrompt) -eq "no" -or ($UserSearchFilterPrompt) -eq "n") {
--- Active Directory/ModuleJacobsADFunctions.psm1
+++ Active Directory/ModuleJacobsADFunctions.psm1
@@ -20,7 +20,7 @@ foreach($x in $outer){
    function Find-ADUser {
         ${ADUserFirstName} = Read-Host -Prompt "Please provide the first name of the Active Directory user you are searching for"
         "`nYour results are being produced"
-        Get-ADUser -Filter "givenname -like '$ADUserFirstName'" -Properties "Description","OfficePhone","telephoneNumber"
+        Get-ADUser -Filter "givenname -like '$ADUserFirstName'" -Properties "Department","Description","OfficePhone","telephoneNumber"
 # User has option to enter their own filter option with the associated info if they require more searching
         ${UserSearchFilterPrompt} = Read-Host -Prompt "`nIs there a different filter you would like to use?"
 
@@ -28,7 +28,7 @@ foreach($x in $outer){
             ${ADUserFilter} = Read-Host -Prompt "`nPlease enter the name of the filter type you would like to use (E.g. surname, department, samaccountname)" 
             ${ADUserFilterInfo} = Read-Host -Prompt "Type the information for your selected filter"
             "`nYour results are being produced`n"
-            Get-ADUser -Filter {$ADUserFilter -like $ADUserFilterInfo} -Properties "Description","OfficePhone","telephoneNumber"
+            Get-ADUser -Filter {$ADUserFilter -like $ADUserFilterInfo} -Properties "Department","Description","OfficePhone","telephoneNumber"
             "`nSearch complete! Hope this was helpful."
             } 
             elseif (($UserSearchFilterPrompt) -eq "no" -or ($UserSearchFilterPrompt) -eq "n") {
