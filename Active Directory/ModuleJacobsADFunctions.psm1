f4b92d7a7759c51d274cf2115c7410c62f72cc5d
JacobNM <133252538+JacobNM@users.noreply.github.com>
2023-11-14 09:58:30 -0500

    Prompts & notifications improved

--- Active Directory/ModuleJacobsADFunctions.psm1
+++ Active Directory/ModuleJacobsADFunctions.psm1
@@ -18,7 +18,7 @@ foreach($x in $outer){
                     }
     }
     function Find-ADUser {
-        ${ADUserFirstName} = Read-Host -Prompt "Please provide the first name of the Active Directory user you are searching for"
+        ${ADUserFirstName} = Read-Host -Prompt "`nPlease provide the first name of the Active Directory user you are searching for"
         "`nYour results are being produced"
         Get-ADUser -Filter {givenname -eq $ADUserFirstName} -Properties "Department","Description","telephoneNumber","OfficePhone" | 
         Select-Object department,Description,distinguishedname,enabled,objectclass,OfficePhone,telephoneNumber,samaccountname,UserPrincipalName,
@@ -26,23 +26,28 @@ foreach($x in $outer){
             @{Name = "Last Name"; Expression = {$_.surname}} | 
         Format-List "First Name","Last Name",Description,Department,OfficePhone,telephoneNumber,samaccountname,UserPrincipalName,distinguishedname,objectclass,enabled
         
+        "`nSearch complete."
+
         # User has option to enter another first name if they require more searching
-        ${ADUserFirstNameRepeatSearch} = Read-Host -Prompt "Would you like to search another user first name?"
+        ${ADUserFirstNameRepeatSearch} = Read-Host -Prompt "Would you like to search another user first name?`n[Y] Yes [N] No"
         
         #User will have the opportunity to continue searching by first name so long as they continue to answer "yes" when prompted
         while (($ADUserFirstNameRepeatSearch) -eq "yes" -or ($ADUserFirstNameRepeatSearch) -eq "y" ) {
-            ${ADUserFirstNameNextSearch} = Read-Host -Prompt "Please provide the first name of the Active Directory user you are searching for"
+            ${ADUserFirstNameNextSearch} = Read-Host -Prompt "`nPlease provide the first name of the Active Directory user you are searching for"
             Get-ADUser -Filter {givenname -eq $ADUserFirstNameNextSearch} -Properties "Department","Description","telephoneNumber","OfficePhone" |
             Select-Object department,Description,distinguishedname,enabled,objectclass,OfficePhone,telephoneNumber,samaccountname,UserPrincipalName,
                 @{Name="First Name";Expression = {$_.givenname}},
                 @{Name = "Last Name"; Expression = {$_.surname}} | 
             Format-List "First Name","Last Name",Description,department,OfficePhone,telephoneNumber,samaccountname,UserPrincipalName,distinguishedname,objectclass,enabled
-            ${ADUserFirstNameRepeatSearch} = Read-Host -Prompt "Would you like to search another user first name?";
+            
+            "`nSearch complete."
+
+            ${ADUserFirstNameRepeatSearch} = Read-Host -Prompt "Would you like to search another user first name?`n[Y] Yes [N] No";
         }
             
         # Provides opportunity to search AD using a different property as the filter
         if (($ADUserFirstNameRepeatSearch) -eq "No" -or ($ADUserFirstNameRepeatSearch) -eq "n") {
-            ${UserSearchFilterPrompt} = Read-Host -Prompt "`nIs there a different filter you would like to use?"
+            ${UserSearchFilterPrompt} = Read-Host -Prompt "`nIs there a different filter you would like to use?`n[Y] Yes [N] No"
             }    
         #User will have the opportunity to continue searching by first name so long as they continue to answer "yes" when prompted   
         while (($UserSearchFilterPrompt) -eq "yes" -or ($UserSearchFilterPrompt) -eq "y") {
@@ -56,7 +61,7 @@ foreach($x in $outer){
             Format-List "First Name","Last Name",Description,Department,OfficePhone,telephoneNumber,samaccountname,UserPrincipalName,distinguishedname,objectclass,enabled
             "Search complete.`n"
             #Gives user the opportunity to continue searching using a custom filter
-            ${UserSearchFilterPrompt} = Read-Host -Prompt "Would you like to search another filter type?"
+            ${UserSearchFilterPrompt} = Read-Host -Prompt "Would you like to search another filter type?`n[Y] Yes [N] No"
             }
     
         if (($UserSearchFilterPrompt) -eq "no" -or ($UserSearchFilterPrompt) -eq "n") {
